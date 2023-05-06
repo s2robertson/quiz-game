c82d98c429d09ed7e2d0885b87f790947bf724b7
Steven Robertson <s2robertson@gmail.com>
2023-05-06 19:23:11 -0400

    added basic high score functionality

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
 const mainEl = document.querySelector('main');
-const startRootEl = document.getElementById('startRoot');
+const startQuizButton = document.getElementById('startQuizButton');
+const highScoresButton = document.getElementById('highScoresButton');
 
 let quizRootEl;
 let resultsRootEl;
@@ -7,22 +8,21 @@ let highScoresRootEl;
 
 let questions;
 let questionIndex = -1;
+let highScores;
+const MAX_HIGH_SCORES_LENGTH = 10;
 
 let currentScore = 0;
 
-// start quiz button
-document.querySelector('#startRoot button').addEventListener('click', () => {
-    showQuiz();
-});
+startQuizButton.addEventListener('click', showQuiz);
+highScoresButton.addEventListener('click', showHighScores);
 
 function showQuiz() {
     if (!quizRootEl) {
         buildQuizRoot();
     }
-    if (!questions) {
-        loadQuestions();
-    }
     showNextQuestion();
+    startQuizButton.classList.add('hidden');
+    highScoresButton.classList.add('hidden');
     mainEl.replaceChildren(quizRootEl);
 }
 
@@ -58,7 +58,7 @@ function showNextQuestion() {
     for (let i = 0; i < 4; i++) {
         /* li
          *   button */
-        let button = listItems[i].children[0]
+        let button = listItems[i].children[0];
         button.textContent = questions[questionIndex].choices[i];
         button.dataset.value = questions[questionIndex].choices[i];
     }
@@ -71,7 +71,68 @@ function quizMakeChoice(choice) {
     showNextQuestion();
 }
 
+function showHighScores(highlightIndex) {
+    console.log('showing high scores');
+    startQuizButton.classList.remove('hidden');
+    highScoresButton.classList.add('hidden');
+    if (!highScoresRootEl) {
+        buildHighScoresRoot();
+    }
+    mainEl.replaceChildren(highScoresRootEl);
+}
+
+function buildHighScoresRoot() {
+    console.log('building high scores');
+    highScoresRootEl = document.createElement('div');
+    const subheading = document.createElement('h2');
+    subheading.textContent = 'High Scores';
+    
+    if (!highScores) {
+        loadHighScores();
+    }
+    const scoresList = document.createElement('ol');
+    const listItems = highScores.map(({ name, score }) => {
+        const listItem = document.createElement('li');
+        listItem.textContent = `${name}: ${score}`;
+        return listItem;
+    });
+    scoresList.append(...listItems);
+    highScoresRootEl.append(subheading, scoresList);
+}
+
+function loadHighScores() {
+    if (highScores) return;
+
+    highScores = JSON.parse(localStorage.getItem('quiz-game-high-scores'));
+    if (!highScores) {
+        highScores = [];
+    }
+}
+
+function findPlaceInHighScores(score) {
+    let i;
+    for (i = highScores.length - 1; i >= 0; i--) {
+        if (score >= highScores[i].score) {
+            break;
+        }
+    }
+    return i + 1;
+}
+
+function addToHighScores(name, score) {
+    let i = findPlaceInHighScores(score);
+    if (i < MAX_HIGH_SCORES_LENGTH) {
+        highScores = highScores.slice(0, i).concat(
+            { name, score },
+            highScores.slice(i, MAX_HIGH_SCORES_LENGTH - 1)
+        );
+    }
+    localStorage.setItem('quiz-game-high-scores', JSON.stringify(highScores));
+}
+
 function loadQuestions() {
+    if (questions) return;
+
     questions = [{
         question: 'What is the answer (a)?',
         choices: ['a', 'b', 'c', 'd'],
@@ -90,3 +151,7 @@ function loadQuestions() {
         answer: 'd'
     }]
 }
+
+loadQuestions();
+loadHighScores();
+showHighScores();
